85002ddfb83230e02eb895510e4c7864952afad0
cg-zhou <z_cg@foxmail.com>
2018-09-13 18:07:56 +0800

    Add test method for signature api of Wechat

---
+++ web/site-z11g/Utils/Alogrithm.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SiteZ11G.Utils
+{
+    public static class Alogrithm
+    {
+        public static string Sha1(string sourceString)
+        {
+            var bytes = _sha1Encoding.GetBytes(sourceString);
+            var hashBytes = _sha1.ComputeHash(bytes);
+            var hashString = _sha1Encoding.GetString(hashBytes);
+            return hashString;
+        }
+
+        private static SHA1 _sha1 = new SHA1CryptoServiceProvider();
+        private static Encoding _sha1Encoding = Encoding.ASCII;
+    }
+}
