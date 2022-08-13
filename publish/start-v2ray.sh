d1833c9ecb9056a790f9f5f7dafafe0a582e4599
周春光 <9943470+cg-zhou@users.noreply.github.com>
2022-08-13 18:02:06 +0800

    add 'restart always' in v2ray

--- publish/start-v2ray.sh
+++ publish/start-v2ray.sh
@@ -2,4 +2,4 @@ docker image pull v2fly/v2fly-core
 
 docker rm v2ray -f
 
-docker run --name v2ray -p 10086:10086 -d -v $PWD/v2ray/config.json:/etc/v2ray/config.json v2fly/v2fly-core
+docker run --restart=always --name v2ray -p 10086:10086 -d -v $PWD/v2ray/config.json:/etc/v2ray/config.json v2fly/v2fly-core
