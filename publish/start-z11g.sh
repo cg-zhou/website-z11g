33d1354fcdb31ae48779c42a8232378a6a118044
周春光 <9943470+cg-zhou@users.noreply.github.com>
2022-08-15 14:11:12 +0800

    Delete start-z11g.sh

--- publish/start-z11g.sh
+++
@@ -1,5 +0,0 @@
-docker image pull metaphor1990/z11g
-
-docker rm -f z11g-nginx
-
-docker run --restart=always --name z11g-nginx -p 80:80 -p 443:443 -d -v $PWD/nginx:/etc/nginx/conf.d metaphor1990/z11g
