ad4e31b32995c9c7ebed170a35c44466ed078192
周春光 <9943470+cg-zhou@users.noreply.github.com>
2022-08-15 14:11:02 +0800

    Delete start-v2ray.sh

--- publish/start-v2ray.sh
+++
@@ -1,5 +0,0 @@
-docker image pull v2fly/v2fly-core
-
-docker rm v2ray -f
-
-docker run --restart=always --name v2ray -p 10086:10086 -d -v $PWD/v2ray/config.json:/etc/v2ray/config.json v2fly/v2fly-core
