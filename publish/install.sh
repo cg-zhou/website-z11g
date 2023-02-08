a4da2cddf4826ed8196bfdfbdfba83d218e8dfa1
周春光 <9943470+cg-zhou@users.noreply.github.com>
2023-02-08 14:56:02 +0800

    fix v2ray docker script error

--- publish/install.sh
+++ publish/install.sh
@@ -27,10 +27,9 @@ wget https://raw.githubusercontent.com/cg-zhou/website-z11g/master/publish/nginx
 # start docker v2ray
 docker image pull v2fly/v2fly-core
 docker rm v2ray -f
-docker run --restart=always --name v2ray -p 10086:10086 -d -v ~/workspace/v2ray/config.json:/etc/v2ray/config.json v2fly/v2fly-core
+docker run --restart=always --name v2ray -p 10086:10086 -d -v ~/workspace/v2ray/config.json:/etc/v2ray/config.json v2fly/v2fly-core run -c /etc/v2ray/config.json
 
 # start docker z11g
 docker image pull metaphor1990/z11g
 docker rm -f z11g-nginx
 docker run --restart=always --name z11g-nginx -p 80:80 -p 443:443 -d -v ~/workspace/nginx:/etc/nginx/conf.d metaphor1990/z11g
-
